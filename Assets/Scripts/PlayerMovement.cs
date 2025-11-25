9805e72158e5c0c5725f68d64f47c288008e7764
Zeryth658 <filip.boisen@gmail.com>
2025-11-25 15:42:18 +0100

    Visual Studio Code Download
    Ändrade IDE som jag skriver kod i

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,6 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public static Vector2 movement;
-
-    private PlayerInput _playerInput;
-    private InputAction _moveAction;
-
-    void Awake()
-    {
-        _playerInput = GetComponent<PlayerInput>();
-
-        _moveAction = _playerInput.actions["Walk"];
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //Hello
 }
