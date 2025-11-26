209b6404af2a95cd4f7ea0a829bebb9b79370a98
Zeryth658 <filip.boisen@gmail.com>
2025-11-26 13:34:11 +0100

    Rotator script
    Added a parent script for rotation

--- Assets/Scripts/PlayerScripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerScripts/PlayerRotation.cs
@@ -1,20 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerRotation : MonoBehaviour
+public class PlayerRotation : Rotator
 {
-    private void LookAt(Vector3 target)
-    {
-        float lookAngle = AngleBetweenTwoPoints(transform.position, target) + 90;
-
-        transform.eulerAngles = new Vector3(0, 0, lookAngle);
-    }
-
-    private float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
-    {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
-    }
-
     private void OnLook(InputValue value)
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(value.Get<Vector2>());
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    protected void LookAt(Vector3 target)
+    {
+        float lookAngle = AngleBetweenTwoPoints(transform.position, target) + 90;
+
+        transform.eulerAngles = new Vector3(0, 0, lookAngle);
+    }
+
+    private float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
