38dcfd6e05db7059f6ce4783c0e3ec3e3f0f9d66
Zeryth658 <filip.boisen@gmail.com>
2025-12-03 15:36:44 +0100

    Started on healthbar

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
-    public float health = 5f;
+    public float maxHealth = 5f;
+    public float currentHealth;
+
     public Hurtbox hurtbox;
+
+    public void Start()
+    {
+        currentHealth = maxHealth;
+    }
 
     public void TakeDamage(float amount, GameObject source)
     {
-        health -= amount;
+        currentHealth -= amount;
         Debug.Log($"{gameObject.name} took {amount} from {source}");
-        if (health <= 0)
+        if (currentHealth <= 0)
             Die();
     }
 
