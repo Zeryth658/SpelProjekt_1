bb6bdb16ffdd60d07b2073316ebe3f803c9cc77e
Mr.Gumba / Ossian <70374508+MrGumba@users.noreply.github.com>
2025-12-04 13:01:07 +0100

    Fixed so enemys sight isnt from the Weapon

--- Assets/Scripts/EnemyScripts/Weapon/EnemyShooter.cs
+++ Assets/Scripts/EnemyScripts/Weapon/EnemyShooter.cs
@@ -51,10 +51,10 @@ public class EnemyShooter : MonoBehaviour
         
         if (target == null) return false;
         
-      Vector2 dir = (target.position - firePoint.position).normalized;
-      float distance = Vector2.Distance(firePoint.position, target.position);
+      Vector2 dir = (target.position - transform.position).normalized;
+      float distance = Vector2.Distance(transform.position, target.position);
 
-      RaycastHit2D hit = Physics2D.Raycast(firePoint.position, dir, distance, obstacleMask);
+      RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, distance, obstacleMask);
       if (hit.collider != null) return false;
 
       
