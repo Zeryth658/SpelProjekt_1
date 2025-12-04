fb70611e236302a5e3f5a49512a72a31a82d8faa
Alexander Macri <alexander.m.1999@gmail.com>
2025-12-04 14:07:53 +0100

    La till gameplay musiken i scenen och spelarens get hit ljudeffekter

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -9,25 +9,35 @@ public class PlayerHealth : MonoBehaviour, IDamageable
 
     public Hurtbox hurtbox;
 
+    [SerializeField] private AudioSource playerGetHitSound1;
+    [SerializeField] private AudioSource playerGetHitSound2;
+
     public void Start()
     {
         currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
+        //healthBar.SetMaxHealth(maxHealth);
     }
 
     public void TakeDamage(float amount, GameObject source)
     {
+        PlayGetHitSounds();
         currentHealth -= amount;
         Debug.Log($"{gameObject.name} took {amount} from {source}");
         if (currentHealth <= 0)
         {
             Die();
         }
-        healthBar.SetHealth(currentHealth);
+        //healthBar.SetHealth(currentHealth);
     }
 
     void Die()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void PlayGetHitSounds()
+    {
+        playerGetHitSound1.Play();
+        playerGetHitSound2.Play();
     }
 }
