9b05767bde041a06c710ca40d3b659f4b6315f86
Zeryth658 <filip.boisen@gmail.com>
2025-12-09 15:14:18 +0100

    Added Attack Feature
    Cannot move the weapon while attacking

--- Assets/Scripts/PlayerScripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerScripts/PlayerRotation.cs
@@ -4,8 +4,13 @@ using UnityEngine.InputSystem;
 public class PlayerRotation : Rotator
 {
     [SerializeField] private Transform weapon;
+
+    private Attack attack;
+
     private void OnLook(InputValue value)
     {
+        if(attack.IsAttacking)
+            return;
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(value.Get<Vector2>());
         LookAt(mousePosition, weapon.transform);
     }
