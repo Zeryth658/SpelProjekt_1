2b572b9cf34d3fde9d09c9a1ed01ca2f84d9b155
Zeryth658 <filip.boisen@gmail.com>
2025-12-10 12:52:11 +0100

    Updated Player Movement

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,31 +1,50 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    private Vector2 movement;
-    
+    [SerializeField] private float maxSpeed = 5f, acceleration = 50f, deacceleration = 100;
+    [SerializeField] private float currentSpeed = 0f;
+
+    private Vector2 pointerInput, movementInput;
+    public Vector2 PointerInput => pointerInput;
+
+    [SerializeField] private InputActionReference movement, pointerPosition;
+
     private Rigidbody2D myRigidbody;
-    private PlayerInput playerInput;
-    private InputAction moveAction;
     private Animator animator;
 
     private void Awake()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
-        playerInput = GetComponent<PlayerInput>();
         animator = GetComponentInChildren<Animator>();
-
-        moveAction = playerInput.actions["Walk"];
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        movement = moveAction.ReadValue<Vector2>();
+        pointerInput = GetPointerInput();
+        
+        movementInput = movement.action.ReadValue<Vector2>();
 
-        animator.SetFloat("Movement input", Mathf.Abs(movement.magnitude));
+        animator.SetFloat("Movement input", Mathf.Abs(movementInput.magnitude));
 
-        myRigidbody.linearVelocity = movement * moveSpeed;
+        if (movementInput.magnitude > 0 && currentSpeed >= 0)
+        {
+            currentSpeed += acceleration * maxSpeed * Time.deltaTime;
+        }
+        else
+        {
+            currentSpeed -= deacceleration * maxSpeed * Time.deltaTime;
+        }
+        currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
+        myRigidbody.linearVelocity = movementInput * currentSpeed;
+    }
+
+    private Vector2 GetPointerInput()
+    {
+        Vector3 mousePos = pointerPosition.action.ReadValue<Vector2>();
+        mousePos.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(mousePos);
     }
 }
