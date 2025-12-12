69bedc9379553165f6afa3d90a2cd92086c3abfa
Zeryth658 <filip.boisen@gmail.com>
2025-12-12 11:55:29 +0100

    Entry Event tag Compare Update

--- Assets/Scripts/EventScripts/EntryEvent.cs
+++ Assets/Scripts/EventScripts/EntryEvent.cs
@@ -6,10 +6,17 @@ public class EntryEvent : MonoBehaviour
 {
     [Header("Event called when stepping on the trigger")]
     public UnityEvent OnEntryEvent;
+    
+    private TagHandle playerTag;
+
+    public void OnEnable()
+    {
+        playerTag = TagHandle.GetExistingTag("Player");
+    }
 
     public void OnTriggerEnter2D(GameObject other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag(playerTag))
         {
             OnEntryEvent.Invoke();
         }
