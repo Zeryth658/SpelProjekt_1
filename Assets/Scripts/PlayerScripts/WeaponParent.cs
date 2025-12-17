cc121e4bc594f1218229bd8c742403836503abbe
Zeryth658 <filip.boisen@gmail.com>
2025-12-17 12:10:15 +0100

    SoundManager

--- Assets/Scripts/PlayerScripts/WeaponParent.cs
+++ Assets/Scripts/PlayerScripts/WeaponParent.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-[RequireComponent(typeof(AudioSource))]
 public class WeaponParent : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer characterRenderer, weaponRenderer;
@@ -16,20 +15,9 @@ public class WeaponParent : MonoBehaviour
 
     public bool IsAttacking { get; private set;}
 
-    [Header("Add Your Sounds Here")]
-    [SerializeField] private List<AudioClip> sounds = new List<AudioClip>();
-    private AudioSource audioSource;
-    private List<AudioClip> shuffleSounds;
-    private int currentIndex = 0;
-
     public void ResetIsAttacking()
     {
         IsAttacking = false;
-    }
-
-    private void Awake()
-    {
-        ShuffleSounds();
     }
 
     void Update()
@@ -67,7 +55,7 @@ public class WeaponParent : MonoBehaviour
         animator.SetTrigger("Attack");
         IsAttacking = true;
         attackBlocked = true;
-        PlayRandomSound();
+        SoundManager.PlaySound(SoundType.Sword);
         StartCoroutine(DelayAttack());
     }
 
@@ -75,36 +63,5 @@ public class WeaponParent : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
         attackBlocked = false;
-    }
-
-    public void PlayRandomSound()
-    {
-        if(sounds.Count == 0)
-        {
-            Debug.LogWarning("No sounds assigned.");
-            return;
-        }
-
-        if(currentIndex >= shuffleSounds.Count)
-        {
-            ShuffleSounds();
-        }
-
-        audioSource.PlayOneShot(shuffleSounds[currentIndex]);
-        currentIndex++;
-    }
-
-    private void ShuffleSounds()
-    {
-        shuffleSounds = new List<AudioClip>(sounds);
-
-        for (int i = 0; i < shuffleSounds.Count; i++)
-        {
-            int randomIndex = Random.Range(i, shuffleSounds.Count);
-            (shuffleSounds[i], shuffleSounds[randomIndex]) =
-                (shuffleSounds[randomIndex], shuffleSounds[i]);
-        }
-
-        currentIndex = 0;
     }
 }
