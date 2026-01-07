f0d85da35552d75ed662a76430b167f9c9e78088
Zeryth658 <filip.boisen@gmail.com>
2026-01-07 15:59:37 +0100

    Swooshing Fix(I think)

--- Assets/Scripts/PlayerScripts/OnDeath.cs
+++ Assets/Scripts/PlayerScripts/OnDeath.cs
@@ -1,10 +1,27 @@
+using System.Collections;
+using Unity.Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class OnDeath : MonoBehaviour
 {
+    [SerializeField] private CinemachineConfiner2D confiner;
+
     public void OnDeathAnimEvent()
     {  
+        confiner.Damping = 0;
+        confiner.SlowingDistance = 0;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        
+        StartCoroutine(Damping());
+    }
+
+    public IEnumerator Damping()
+    {
+        yield return new WaitForNextFrameUnit();
+        
+        confiner.Damping = 1;
+        confiner.SlowingDistance = 1;
     }
 }
