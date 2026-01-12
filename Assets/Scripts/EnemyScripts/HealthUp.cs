a61d7ee091491f65dc7e5a5f125f171eb1d7baea
GrandBoss77 <markokristic121@gmail.com>
2026-01-12 15:59:13 +0100

    BigChangesHealthEffect

--- Assets/Scripts/EnemyScripts/HealthUp.cs
+++ Assets/Scripts/EnemyScripts/HealthUp.cs
@@ -3,25 +3,14 @@ using UnityEngine;
 
 public class HealthUp : MonoBehaviour
 {
-    [SerializeField] private float ExistanceTime = 0.5f;
-    [SerializeField] private float speed = 0.3f;
-    [SerializeField] private float rightDrift = 0.3f;
-    private float timer = 0f;
-
-    public void OnEnable()
-    {
-        timer = 0f;
-    }
+    [SerializeField] private ParticleSystem healEffect;
 
     public void Update()
     {
-        if (timer >= ExistanceTime)
+        if (!healEffect.isPlaying)
         {
             PoolManager.Despawn(gameObject);
             return;
         }
-        timer += Time.deltaTime;
-        Vector2 direction = Vector2.up + Vector2.right * rightDrift;
-        transform.position += (Vector3)(direction.normalized * speed * Time.deltaTime);
     }
 }
