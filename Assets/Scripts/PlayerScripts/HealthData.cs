fce3a7d32da3cbbd3dc456dab2c5ddcf5ba57c6e
Zeryth658 <filip.boisen@gmail.com>
2026-01-13 15:37:30 +0100

    Fixed Currenthealth bug

--- Assets/Scripts/PlayerScripts/HealthData.cs
+++ Assets/Scripts/PlayerScripts/HealthData.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Player/HealthData")]
 public class HealthData : ScriptableObject
 {
-    public float maxHealth;
+    public float maxHealth = 150;
+    public float lessThanMax = 50;
+
+    [HideInInspector] 
     public float currentHealth;
+
+    public void OnEnable()
+    {
+        currentHealth = maxHealth -= lessThanMax;
+    }
 }
