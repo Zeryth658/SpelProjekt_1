1f41378afc2e987a036fde59850198279e399521
Alexander Macri <alexander.m.1999@gmail.com>
2026-01-13 16:21:16 +0100

    Fixade Fade to black prefab

---
+++ Assets/Scripts/FadeToBlackScript.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeToBlackScript : MonoBehaviour
+{
+    
+    private TagHandle playerTag;
+    private bool collided = false;
+    private bool startTimeFade = false;
+    private float objectX;
+    
+    private Transform playerTransform;
+
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float distanceFadeSpeed;
+    [SerializeField] private float transitionToTimerPoint;
+    [SerializeField] private float timedFadeSpeed;
+    
+    void Start()
+    {
+        objectX = transform.position.x;
+        playerTag = TagHandle.GetExistingTag("Player");
+    }
+
+    void Update()
+    {
+        if (collided)
+        {
+            Color fadeImageColor = fadeImage.color;
+            
+            if (startTimeFade)
+            {
+                fadeImageColor.a += Time.deltaTime * timedFadeSpeed; 
+            }
+            else
+            {
+                fadeImageColor.a = (playerTransform.position.x - objectX) * distanceFadeSpeed;
+                if (fadeImageColor.a > transitionToTimerPoint)
+                {
+                    startTimeFade = true;
+                }
+            }
+            fadeImage.color = fadeImageColor;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            playerTransform = other.transform;
+            collided = true;
+        }
+    }
+}
