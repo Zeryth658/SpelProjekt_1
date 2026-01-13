7248e6690d1e176077ff9f53c3ca5d916f6f9496
Mr.Gumba / Ossian <70374508+MrGumba@users.noreply.github.com>
2026-01-13 13:09:43 +0100

    Added Camera Follow Switch object

---
+++ Assets/Scripts/OnEnter/CameraFollowSwitch.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraFollowSwitch : MonoBehaviour
+{
+    [SerializeField] private bool isFollowing = false;
+    private CinemachineCamera cinCamera; 
+    
+    private CinemachineFollow cinemachineFollow;
+    
+    private void Awake()
+    {
+        cinCamera = FindFirstObjectByType<CinemachineCamera>();
+        cinemachineFollow = cinCamera.GetComponent<CinemachineFollow>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cinemachineFollow.enabled = isFollowing;
+        }
+    }
+}
